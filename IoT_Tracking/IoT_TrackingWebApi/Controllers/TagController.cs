9cff70ad1fb5151f2e937d788801b1f9e7c3b1bd
GokhanTurkyilmaz <gokhan20115006@gmail.com>
2021-07-16 18:45:09 +0300

    Wrote tag controller operation

--- IoT_Tracking/IoT_TrackingWebApi/Controllers/TagController.cs
+++ IoT_Tracking/IoT_TrackingWebApi/Controllers/TagController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IoTBussiness.Abstract;
+using IoTEntities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,43 @@ namespace IoT_TrackingWebApi.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
+        ITagService _tagService;
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+        [HttpGet("getTag")]
+        public ActionResult GetTag(int tagId)
+        {
+            var tag = _tagService.GetTag(tagId);
+            if (tag.Data!=null)
+            {
+                return Ok(tag.Data);
+            }
+            return BadRequest();
+        }
+        [HttpGet("getAllTag")]
+        public ActionResult GetAllTag()
+        {
+            var tags = _tagService.GetAllTags();
+            if (tags.Data.Count>0)
+            {
+                return Ok(tags.Data);
+            }
+            return BadRequest();
+        }
+        [HttpPost("addTag")]
+        public ActionResult AddTag(Tag tag)
+        {
+            var addedTag = _tagService.Add(tag);
+            return Ok(addedTag.Message);
+        }
+        [HttpPost("deleteTag")]
+        public ActionResult DeleteTag(int tagId)
+        {
+            var deleteTag = _tagService.GetTag(tagId);
+            _tagService.Delete(deleteTag.Data);
+            return Ok();
+        }
     }
 }
